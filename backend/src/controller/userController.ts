7fe83a04c1ff5ede73f0de5d60cac4a72f1900ee
wesley arizio <45431385+Wesley-Arizio@users.noreply.github.com>
2020-10-30 12:18:51 -0300

    adding user migration, model and controller

---
+++ backend/src/controller/userController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+
+import User from '../model/users';
+import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+
+export default {
+    async createAccount(request: Request, response: Response) {
+        const {
+            name,
+            email,
+            password
+        } = request.body;
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).max(16).required()
+        });
+
+        await schema.validate({
+            name,
+            email,
+            password
+        }, { abortEarly: false });
+
+        const userRepository = getRepository(User);
+
+        const userExists = await userRepository
+            .createQueryBuilder('user')
+            .where("user.email = :email", { email: email })
+            .getOne();
+
+        if(userExists){
+            return response.status(401).send({
+                message: "Invalid credentials"
+            });
+        }
+
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = password;
+
+        await userRepository.save(user);
+
+        return response.status(201).send({ user });
+    }
+}
---
+++ backend/src/model/users.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate, OneToOne, JoinColumn  } from 'typeorm';
+import bcrypt from 'bcrypt';
+
+import Lender from './lenders';
+
+@Entity('users')
+export default class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 10);
+    }
+
+    @OneToOne(type => User, lender => Lender)
+    @JoinColumn()
+    lender: Lender
+}
