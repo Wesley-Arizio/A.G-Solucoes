70adc33ed7d7f0e4bdc5996ea786d7fa910a1a74
wesley arizio <45431385+Wesley-Arizio@users.noreply.github.com>
2020-10-30 13:37:56 -0300

    add authentication route and middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,10 @@
-import { Router, Request, Response } from 'express';
-import LendersController from './controller/lendersController';
+import { Router } from 'express';
 
+import LendersController from './controller/lendersController';
+import UserController from './controller/userController';
+import AuthController from './controller/authController';
+
+import auth from './middleware/auth';
 
 const routes  = Router();
 
@@ -9,5 +13,12 @@ routes.post('/lenders', LendersController.create);
 routes.get('/lenders', LendersController.index);
 
 routes.get('/lender/:id', LendersController.show);
+
+routes.post('/user', UserController.createAccount);
+
+routes.get('/user/auth', AuthController.authenticate);
+
+routes.use(auth);
+
 
 export default routes;
